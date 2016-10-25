0431829c016839af6085e3e8541e572ca4e2c983
Ryan Westlake <ryancwestlake@gmail.com>
2016-10-24 21:38:37 -0600

    initial commit - created react app and set up first 3 components

---
+++ src/App.js
@@ -0,0 +1,43 @@
+import React, { Component } from 'react' //eslint-disable
+import ReactDOM from 'react'
+import KanbanBoard from './components/KanbanBoard.jsx'
+import './App.css'
+
+export default class App extends Component {
+  
+}
+
+let cardsList = [
+  {
+    id: 1,
+    title: 'Read the Book',
+    description: 'I should read the whole book',
+    status: 'in-progress',
+    tasks: []
+  },
+  {
+    id: 2,
+    title: 'Write some code',
+    description: 'Code along with the samples in the book',
+    status: 'todo',
+    tasks: [
+      {
+        id: 1,
+        name: 'ContactList Example',
+        done: true
+      },
+      {
+        id: 2,
+        name: 'Kanban Example',
+        done: false
+      },
+      {
+        id: 3,
+        name: 'My own experiments',
+        done: false
+      }
+    ]
+  },
+];
+
+ReactDOM.render(<KanbanBoard cards={cardsList} />, document.getElementById('root'));
